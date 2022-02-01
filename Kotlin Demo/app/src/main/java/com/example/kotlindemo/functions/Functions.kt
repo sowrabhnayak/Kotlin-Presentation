53057d65826f69b9df8288c41189e5a90febce2f
Nayak Sowrabh <nayak.sowrabh@inmobi.com>
2022-02-01 11:10:15 +0530

    * Updated functions with variable setter and lazy properties

--- Kotlin Demo/app/src/main/java/com/example/kotlindemo/functions/Functions.kt
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/functions/Functions.kt
@@ -2,6 +2,17 @@ package com.example.kotlindemo.functions
 
 class Functions {
 
+    private var variable = ""
+        set(value) {
+            println("Set called on variable with value $value")
+            field = value.lowercase().trim()
+            println("Updated the variable value to $field")
+        }
+
+    private val os by lazy {
+         "${System.getProperty("os.name")}v${System.getProperty("os.version")}"
+    }
+
     private fun String.responseIsValid(): Boolean {
         return isNullOrBlank().not()
     }
