ab5fe1c733d17cdcd06813b40f2be10ed337f27e
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 04:00:28 +0530

    Corrected Extention Property Example

--- Kotlin Demo/app/src/main/java/com/example/kotlindemo/extenstions/ExtentionProperty.kt
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/extenstions/ExtentionProperty.kt
@@ -3,9 +3,10 @@ package com.example.kotlindemo.extenstions
 val x = 10;
 
 fun main(){
-    println("Binary Form of $x = ${x.binaryForm()}")
+    println("Binary Form of $x = ${x.binaryForm}")
 }
 
-fun Int.binaryForm():String{
-    return this.toString(2);
+val Int.binaryForm:String
+get(){
+    return toString(2)
 }
