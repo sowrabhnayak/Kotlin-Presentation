e6730dd25556322dfde856bf43c7d7b5f8feec19
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 02:29:21 +0530

    Modified Concise

---
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/concise/DemoClass.kt
@@ -0,0 +1,12 @@
+package com.example.kotlindemo.concise
+
+val _Int = 0
+var _String = "String"
+
+val optionalInt:Int? = null
+var optionalString:String? = "String"
+
+fun main(){
+    optionalString = _String
+    _String = optionalString
+}
---
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/concise/JavaDemoClass.java
@@ -0,0 +1,28 @@
+package com.example.kotlindemo.concise;
+
+public class JavaDemoClass {
+
+    private String firstName;
+    private String lastName;
+
+    public JavaDemoClass(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
