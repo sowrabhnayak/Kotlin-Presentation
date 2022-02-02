85f700d4acc9b178d7024c1b5bd7d0be8dfe01cc
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 03:44:01 +0530

    Added code to show Class Independent Functions

--- Kotlin Demo/app/src/main/java/com/example/kotlindemo/concise/DemoClass.kt
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/concise/DemoClass.kt
@@ -6,7 +6,7 @@ var _String = "String"
 val optionalInt:Int? = null
 var optionalString:String? = "String"
 
-fun main(){
-    optionalString = _String
+//fun main(){
+//    optionalString = _String
 //    _String = optionalString
-}
+//}
