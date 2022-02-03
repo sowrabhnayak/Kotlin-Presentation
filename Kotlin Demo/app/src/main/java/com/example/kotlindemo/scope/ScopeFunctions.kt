890cb0ee8543070129a3541616689e5b96d58f07
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 07:34:11 +0530

    Corrected Scoping

--- Kotlin Demo/app/src/main/java/com/example/kotlindemo/scope/ScopeFunctions.kt
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/scope/ScopeFunctions.kt
@@ -39,12 +39,12 @@ class ScopeFunctions {
     }
 
     fun withFunction(view: ViewDetails) {
-        with(view) {
-            println(
-                "The shape has length of $length breadth of $breadth and is a " +
-                        (if (isSquare) "square" else "rectangle") + " with area $area"
-            )
-        }
+        println("The shape has length of ${view.length} breadth of ${view.breadth} and is a " +
+                    (if (view.isSquare) "square" else "rectangle") + " with area $${view.area}")
+//        with(view) {
+//            println("The shape has length of $length breadth of $breadth and is a " +
+//                        (if (isSquare) "square" else "rectangle") + " with area $area")
+//        }
     }
 
 
