1c54efa15addbbe801c739d5b9d3ed9f60858900
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 07:23:38 +0530

    Added Aggressive Lambda example

--- Kotlin Demo/app/src/main/java/com/example/kotlindemo/functions/Functions.kt
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/functions/Functions.kt
@@ -1,18 +1,51 @@
 package com.example.kotlindemo.functions
 
-class Functions {
+import android.os.Build
+import android.view.View
+import android.view.View.GONE
+import android.widget.Button
+import android.widget.Spinner
+import androidx.annotation.RequiresApi
 
-    fun processResponse(response: String, callback: (ResponseEnum) -> Unit) {
-        if (response.isBlank().not()) {
-            callback(ResponseEnum.SUCCESS)
-        } else {
-            callback(ResponseEnum.FAILURE)
-        }
-    }
 
-}
-
+/**
+ * Receive
+ */
 enum class ResponseEnum {
     SUCCESS,
     FAILURE
+}
+fun processResponse(response: String, callback: (ResponseEnum) -> Unit) {
+    if (response.isBlank().not()) {
+        callback(ResponseEnum.SUCCESS)
+    } else {
+        callback(ResponseEnum.FAILURE)
+    }
+}
+
+fun main() {
+    processResponse("") { println(it) }
+
+//    processResponse("") { responseEnum, responseCode -> println("$responseEnum $responseCode") }
+}
+
+
+
+
+
+
+
+lateinit var button:Button
+lateinit var spinner:Spinner
+
+// Kotlin Style
+val onLongClickListener: (View) -> Boolean = {
+    it.visibility = GONE
+    true
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun setListener(){
+    button.setOnLongClickListener(onLongClickListener)
+    button.setOnContextClickListener(onLongClickListener)
 }
---
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/functions/FunctionsJava.java
@@ -0,0 +1,64 @@
+package com.example.kotlindemo.functions;
+
+import static android.view.View.GONE;
+
+import android.os.Build;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.function.Consumer;
+
+public class FunctionsJava {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static void processResponse(String response, Consumer<ResponseEnum> callback) {
+        if (!response.trim().isEmpty()) {
+            callback.accept(ResponseEnum.SUCCESS);
+        } else {
+            callback.accept(ResponseEnum.FAILURE);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static void main(String[] args){
+        processResponse("", new Consumer<ResponseEnum>() {
+            @Override
+            public void accept(ResponseEnum responseEnum) {
+                System.out.println(responseEnum);
+            }
+        });
+//        processResponse("", new Callback<ResponseEnum>() {
+//            @Override
+//            public void onResponseParsed(ResponseEnum responseEnum) {
+//                System.out.println(responseEnum);
+//            }
+//        });
+    }
+
+    interface Callback<T> {
+        public void onResponseParsed(T t);
+    }
+
+
+
+
+
+
+    Button button;
+    Spinner spinner;
+
+    // Java Style
+    View.OnLongClickListener onLongClickListener = v -> {
+        v.setVisibility(GONE);
+        return true;
+    };
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    public void setListener(){
+        button.setOnLongClickListener(onLongClickListener);
+//        button.setOnContextClickListener(onLongClickListener)
+    }
+}
