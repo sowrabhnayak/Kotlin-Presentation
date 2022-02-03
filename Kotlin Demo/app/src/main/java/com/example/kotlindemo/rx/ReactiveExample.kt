61db25a15da92603582d8b0fc2e43b474938a7f5
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 09:20:21 +0530

    Added Reactive Paradigmg

---
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/rx/ReactiveExample.kt
@@ -0,0 +1,54 @@
+package com.example.kotlindemo.rx
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+val doughProducer: Flow<List<Dough>> = flow{
+    var doughCount = 0;
+    while(true){
+        delay(1000)
+        val rand = Math.ceil(Math.random()*10).toInt()
+        val list = mutableListOf<Dough>()
+        for(i in 1 until rand+1){
+            list.add(Dough(doughCount++))
+        }
+        println("Dough Emitted ${doughCount-rand} - ${doughCount}")
+        emit(list)
+    }
+}
+
+val cheeseProducer: Flow<Cheese> = flow{
+    var cheeseCount = 0;
+    while(true){
+        delay(600)
+        println("Cheese Emitted $cheeseCount")
+        emit(Cheese(cheeseCount++))
+    }
+}
+
+val pizzaBread:Flow<PizzaBread> = doughProducer.transform { doughList ->
+    doughList.forEach{ dough ->
+        delay(100)
+        println("PizzaBread emitted ${dough.index}")
+        emit(PizzaBread(dough.index))
+    }
+}
+
+val pizza:Flow<Pizza> = pizzaBread.zip<PizzaBread, Cheese, Pizza>(cheeseProducer) { pizzaBread,cheese ->
+    val index = Math.min(pizzaBread.index,cheese.index)
+    println("Pizza emitted $index")
+    Pizza(index)
+}
+
+suspend fun main(){
+    makePizzas()
+}
+
+suspend fun makePizzas() {
+    pizza.collect { println("Pizza ${it.index}") }
+}
+
+class Dough(val index:Int)
+class Cheese(val index:Int)
+class PizzaBread(val index:Int)
+class Pizza(val index:Int)
