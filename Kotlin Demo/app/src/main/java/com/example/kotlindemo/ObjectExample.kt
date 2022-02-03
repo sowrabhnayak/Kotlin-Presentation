6899557c027eb42fc16585d45fceb3bedab93ff6
Sunil Kumar Reddy Kuna <sunil.reddy@inmobi.com>
2022-02-03 05:35:06 +0530

    Added Object example

---
+++ Kotlin Demo/app/src/main/java/com/example/kotlindemo/ObjectExample.kt
@@ -0,0 +1,14 @@
+package com.example.kotlindemo
+
+object Preferences {
+    fun getSetting1(){}
+    fun getSetting2(){}
+    fun getSetting3(){}
+}
+
+
+fun main(){
+    println(Preferences.getSetting1())
+    println(Preferences.getSetting2())
+    println(Preferences.getSetting3())
+}
